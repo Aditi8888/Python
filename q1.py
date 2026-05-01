945b1b8fa1eab9ce0270c050cd567656dce0fef8
Aditi8888 <88945828+Aditi8888@users.noreply.github.com>
2026-05-01 06:28:45 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,9 @@
+name = input("enter your name :")
+print(f"Hello {name}")
+age = input("enter your age :")
+print(f"You are {age}years old")
+city = input("enter your city")
+print(f"you live in {city}")
+print(f"Hello {name}"+ f"You are {age}years old"+ f"You live in {city}")
+
+
