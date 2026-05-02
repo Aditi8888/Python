0ec6d3221ba3e4a42eb9f3c45bfa0f43c46c8afd
Aditi8888 <88945828+Aditi8888@users.noreply.github.com>
2026-05-02 15:34:48 +0530

    Add files via upload

---
+++ q6.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number :"))
+if number%2 == 0:
+    print("it is even ")
+else:
+    print("the number is odd")
+if number%5 == 0 :
+    print("the number is divisible.")   
+else :
+    print("it is not divisible")  
---
+++ q7.py
@@ -0,0 +1,6 @@
+for number in range (1,51) :
+    print(number)
+    if number%2==0 :
+        print("the numbers are even")
+    else:
+        print("the numbers are odd")
